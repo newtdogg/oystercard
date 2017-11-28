f2bc4bb7f51d18e0fd39b3e2a48d103779f2b6d1
newtdogg <edwardfrancisnewton@live.com>
2017-11-28 15:53:18 +0000

    refactoring

--- featuretests.rb
+++ featuretests.rb
@@ -1,4 +1,5 @@
 require './lib/oystercard.rb'
 card = OysterCard.new
 card.top_up(10)
-p card.touch_in(aldgate)
+p card.touch_in('ted')
+p card.in_journey?
--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -15,16 +15,14 @@ class OysterCard
   def touch_in(station)
     raise 'already in journey!' if in_journey? == true
     raise 'card balance under minimum of £1' if @balance < MIN_BALANCE
-    @in_journey = true
     @entry_station = station
   end
   def in_journey?
-    @in_journey
+    @entry_station != nil ? @in_journey = true : @in_journey = false
   end
   def touch_out
     raise 'journey not started, cant touch out!' if in_journey? == false
     deduct(5)
-    @in_journey = false
     @entry_station = nil
   end
 
