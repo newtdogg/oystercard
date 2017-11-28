5307ee0c8d795a31b5338c52f818c29db30ddb97
newtdogg <edwardfrancisnewton@live.com>
2017-11-28 11:28:25 +0000

    created an ability for the card to touch in and out of journeys

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,25 @@
 class OysterCard
 
-  attr_reader :balance
+ attr_reader :balance, :in_journey
   CARD_LIMIT = 90
   def initialize
     @balance = 0
+    @in_journey = false
   end
   def top_up(amount)
     raise 'card max of £90 exceeded' if @balance + amount > CARD_LIMIT
     @balance += amount
   end
-
+  def deduct(amount)
+    @balance -= amount
+  end
+  def touch_in
+    @in_journey = true
+  end
+  def in_journey?
+    @in_journey
+  end
+  def touch_out
+    @in_journey = false
+  end
 end
